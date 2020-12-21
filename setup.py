266fd8d74b73a73293d001485eef6b32620dbf9b
AlbertLis <bercik828@o2.pl>
2020-12-21 17:41:57 +0100

    add correct version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='keras_conv_visualizer',
-    version='0.0.1',
+    version='0.0.2',
     author="Albert Lis",
     author_email="albert.lis.1996@gmail.com",
     description="Package allows visualize convolutional layers from keras models.",
